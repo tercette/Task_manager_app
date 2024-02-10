151954c61b07dda042f0e4fcdb985e3a7ea51058
Leandro Tercette <leandro.tercette@cnj.jus.br>
2024-02-10 03:01:23 -0300

    styling the modal via tailwind and implementing the cancel button into newProject

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -2,7 +2,7 @@ import Input from "./Input";
 import { Fragment, useRef } from "react";
 import Modal from "../components/Modal";
 
-export default function NewProject({ onAdd }) {
+export default function NewProject({ onAdd, onCancel }) {
   const modal = useRef();
 
   const title = useRef();
@@ -33,14 +33,14 @@ export default function NewProject({ onAdd }) {
   return (
     <>
       <Modal ref={modal} buttonCaption="Okay">
-        <h2>Invalid Input</h2>
-        <p>Ops ... looks like you forgot to enter a value</p>
-        <p>Please make sure you provide a valid value for every input field.</p>
+        <h2 className="text-xl font-bold text-stone-500 my-4">Invalid Input</h2>
+        <p className="text-stone-600 mb-4">Ops ... looks like you forgot to enter a value</p>
+        <p className="text-stone-600 mb-4">Please make sure you provide a valid value for every input field.</p>
       </Modal>
       <div className="w-[35rem] mt-16">
         <menu className="flex items-center justify-end gap-4 my-4">
           <li>
-            <button className="text-stone-800 hover:text-stone-950">
+            <button className="text-stone-800 hover:text-stone-950" onClick={onCancel} >
               Cancel
             </button>
           </li>
